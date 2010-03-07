de780d44190267cd55e1242606bce79da5370013
Sean Sullivan <sean@seansullivan.com>
2010-03-07 10:28:36 -0800

    get username and password from system properties

--- src/test/java/UrbanAirshipClientTest.java
+++ src/test/java/UrbanAirshipClientTest.java
@@ -5,10 +5,29 @@ import urbanairship.*;
 
 public class UrbanAirshipClientTest
 {
+	private String username = "username";
+	private String password = "password";
+	
+	@BeforeTest
+	public void setUp()
+	{
+		String usernameProperty = (String) System.getProperties().get("urbanairship.java.username");
+		if (usernameProperty != null)
+		{
+			username = usernameProperty;
+		}
+		
+		String passwordProperty = (String) System.getProperties().get("urbanairship.java.password");
+		if (passwordProperty != null)
+		{
+			password = passwordProperty;
+		}
+	}
+	
 	@Test
 	public void client()
 	{
-		UrbanAirshipClient client = new UrbanAirshipClient(true, "user1", "pass1");
+		UrbanAirshipClient client = new UrbanAirshipClient(true, username, password);
 		
 		DeviceToken dtoken = new DeviceToken();
 		dtoken.setToken("dtoken123-" + System.currentTimeMillis());
