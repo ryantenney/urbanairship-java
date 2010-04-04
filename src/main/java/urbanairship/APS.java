bcc3bcf8cb2032b19105e6788e6577257502df24
Sean Sullivan <sean@seansullivan.com>
2010-04-03 19:25:25 -0700

    support Urban Airship's autobadge feature

--- src/main/java/urbanairship/APS.java
+++ src/main/java/urbanairship/APS.java
@@ -1,20 +1,67 @@
 
 package urbanairship;
 
+/**
+ * 
+ * APS: Apple Push Service
+ *
+ */
 public class APS implements java.io.Serializable
 {
-	private Integer badge;
+	private Object badge;
 	private String alert;
 	private String sound;
 	
-	public Integer getBadge()
+	public Integer getBadgeAsInteger()
 	{
-		return badge;
+		if (this.badge instanceof Integer)
+		{
+			return (Integer) badge;
+		}
+		else
+		{
+			throw new IllegalStateException("badge is not an integer");
+		}
 	}
 	
-	public void setBadge(Integer badge)
+	
+	public String getBadgetAsString()
 	{
-		this.badge = badge;
+		if (this.badge instanceof String)
+		{
+			return (String) badge;
+		}
+		else
+		{
+			throw new IllegalStateException("badge is not a string");
+		}
+	}
+	
+	/**
+	 * 
+	 * use this method for autobadge
+	 * 
+	 * @param badge  "auto"   or "+1"  or "-1"
+	 * 
+	 * @see #setBadge(Integer)
+	 * 
+	 */
+	public void setBadge(String strBadge)
+	{
+		if ( (strBadge != null) 
+				&& (!strBadge.equalsIgnoreCase("auto"))
+				&& (!strBadge.startsWith("+"))
+				&& (!strBadge.startsWith("-")) )
+		{
+			throw new IllegalArgumentException("strBadge parameter: " + strBadge);
+		}
+		
+		this.badge = strBadge;
+	}
+	
+	public void setBadge(Integer iBadge)
+	{
+		this.badge = iBadge;
 	}
 	
 	public String getAlert()
