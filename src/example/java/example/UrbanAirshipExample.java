b1c73e2d1c5a7b446e8b08e0f4f2ee9b6a8cae31
Sean Sullivan <sean@seansullivan.com>
2011-06-03 10:24:55 -0700

    tweaked example code.

--- src/example/java/example/UrbanAirshipExample.java
+++ src/example/java/example/UrbanAirshipExample.java
@@ -11,26 +11,29 @@ public class UrbanAirshipExample
 	public void doSomething()
 	{
 		
-		UrbanAirshipClient client = new UrbanAirshipClient("urbanUsername", "urbanPassword");
+		UrbanAirshipClient client = new UrbanAirshipClient("uaAccount", "uaPass");
 		
 		Device device = new Device();
 		
 		device.setiOSDeviceToken("iosDeviceToken1");
+		
 		device.setAlias("giltUser58");
 		
-		device.addTag("idaho_residents");
-		device.addTag("rebecca_black_fans");
+		device.addTag("oregon_resident");
+		device.addTag("pacific_time_zone");
+		device.addTag("rebecca_black_fan");
 		
 		client.register(device);
 		
 		Push p = new Push();
 		
-		p.addTag("rebecca_black_fans");
+		p.addTag("rebecca_black_fan");
 		
 		p.setMessage("Yesterday was Thursday. Today it is Friday.");
 		
 		Calendar scheduleFor = Calendar.getInstance();
 		scheduleFor.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+		scheduleFor.set(Calendar.HOUR_OF_DAY, 8);
 		
 		p.addScheduleFor(scheduleFor);
 		
